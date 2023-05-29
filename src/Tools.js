391aa908fc435366c1473688c3fbfa8b1c4e8521
User <User@DESKTOP-C51DDIB>
2023-05-29 14:02:50 +0500

    background and container

---
+++ src/Tools.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+const Tools = () => {
+  return (
+    <Container sx={{backgroundColor: 'white'}}>
+        <h1>hello</h1>
+
+    </Container>
+  )
+}
+
+export default Tools
