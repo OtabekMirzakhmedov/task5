d4c1cc364456701904ead8c5cebbcb665d9c7d94
User <User@DESKTOP-C51DDIB>
2023-05-30 18:17:41 +0500

    toolbar done

--- src/Tools.js
+++ src/Tools.js
@@ -7,10 +7,33 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
 
-const Tools = ({onGenerateTable}) => {
+const Tools = () => {
     const [country, setCountry] = useState('');
   const [seed, setSeed] = useState('');
+  const [sliderValue, setSliderValue] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+  const [error, setError] = useState(0);
+
+
+  const handleRandomSeed = () => {
+    const randomSeed = Math.floor(Math.random() * 1000) + 1;
+    setSeed(randomSeed.toString());
+  };
+
+  const handleSliderChange = (event, newValue) => {
+    setSliderValue(newValue);
+    setInputValue(newValue*100);
+    setError(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    const value = Number(event.target.value);
+    setInputValue(value);
+    setError(value / 100);
+    setSliderValue(value/100);
+  };
 
   const handleCountryChange = (event) => {
     setCountry(event.target.value);
@@ -19,49 +42,64 @@ const Tools = ({onGenerateTable}) => {
   const handleSeedChange = (event) => {
     setSeed(event.target.value);
   };
-  const handleGenerateTable = () => {
-    onGenerateTable(country, seed); // Emit the country and seed values
-  };
+
 
   return (
-    <Container sx={{backgroundColor: 'white', marginTop: '20px'}}>
-         <Box sx={{
-            display: 'flex',
-            justifyItems: 'start',
-            width: 'fit-content',
-            margin: '0 auto',
-            padding: '5px'
-        }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Country</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={country}
-          label="Age"
-          onChange={handleCountryChange}
-          sx={{width: '150px'}}
-        >
-          <MenuItem value={"Spain"}>Spain</MenuItem>
-          <MenuItem value={"Poland"}>Poland</MenuItem>
-          <MenuItem value={"Uzbekistan"}>Uzbekistan</MenuItem>
-        </Select>
-        
-      </FormControl>
-      <TextField
-          label="Seed Value"
-          value={seed}
-          onChange={handleSeedChange}
-        />
-        <Button variant="outlined" >Random</Button>
-
-
-    </Box>
-    <Box>
-    <Button variant="contained" onClick={handleGenerateTable}>Generate Table</Button>
-    </Box>
-    <p>{country}</p>
-    <p>{seed}</p>
+    <Container sx={{ backgroundColor: 'white', marginTop: '20px' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '10px',
+          width: 'fit-content',
+          alignItems: 'center'          
+        }}
+      >
+        <Box sx={{ padding: '5px', margin: '0 auto'  }}>
+          <FormControl sx={{ width: '150px' }}>
+            <InputLabel>Select Country</InputLabel>
+            <Select
+              value={country}
+              label="Select Country"
+              onChange={handleCountryChange}
+              size="small"
+            >
+              <MenuItem value={'spain'}>Spain</MenuItem>
+              <MenuItem value={'poland'}>Poland</MenuItem>
+              <MenuItem value={'germany'}>Germany</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <Box sx={{ display: 'flex', alignContent: 'center',  padding: '5px', gap: '5px', alignItems: 'center', margin: '0 auto' }}>
+          <TextField
+            label="Seed Value"
+            value={seed}
+            onChange={handleSeedChange}
+            size="small"
+            sx={{ width: '150px' }}
+          />
+          <Button variant="outlined" size="small" onClick={handleRandomSeed}>
+          Random seed
+        </Button>
+        </Box>
+        <Box sx={{ padding: '10px' , display: 'flex', width:'600px', gap: '10px'}}>
+          <Slider
+            value={sliderValue}
+            onChange={handleSliderChange}
+            min={0}
+            max={10}
+            step={0.25}
+            marks={[{ value: 0, label: '0' }, { value: 10, label: '10' }]}
+            valueLabelDisplay="auto"
+            size="medium"
+          />
+          <TextField
+            label="Error"
+            value={inputValue}
+            onChange={handleInputChange}
+            size="small"
+          />
+        </Box>
+      </Box>
 
     
 
